94cc81a49eb7e6017134a6f8ba278a95d7714530
Salto3 <Salto3@CHSA.local>
2023-05-03 18:31:51 -0300

    Add Trophy Values

--- js/crud.js
+++ js/crud.js
@@ -5,30 +5,46 @@ function cadastrar() {
     const valor = document.querySelector("#valor").value
     const descricao = document.querySelector("#descricao").value
     const estilo = document.querySelector("#estilo").value
+    const imagemfundo = document.querySelector("#bg-image").value
+    const platina = document.querySelector("#tf-platina").value
+    const ouro = document.querySelector("#tf-ouro").value
+    const prata = document.querySelector("#tf-prata").value
+    const bronze = document.querySelector("#tf-bronze").value
 
     const jogo = {
         nome,
         valor,
         descricao,
-        estilo
+        estilo,
+        imagemfundo,
+        platina,
+        ouro,
+        prata,
+        bronze
     }
 
     document.querySelector("#jogos").innerHTML += criarCard(jogo)
 }
 
-function apagar(botao){
+function apagar(botao) {
     console.log(botao)
     botao.parentNode.parentNode.parentNode.remove()
 }
 
 function criarCard(jogo) {
     const card = `
-    <div class="card col-lg-3 col-md-6 col-sm-12 text-bg-dark p-3">
-    <div class="card-header" id="titulo-jogo">
-        ${jogo.nome}
-    </div>
+    <div class="card col-lg-3 col-md-6 col-sm-12 text-bg-dark p-3" style="background-image: url(${jogo.imagemfundo});">
+    <div class="card-header" id="titulo-jogo">${jogo.nome}</div>
     <div class="card-body">
         <p class="card-text">${jogo.descricao}</p>
+        <p>
+            <ul>
+                <li><i class="bi bi-trophy-fill" id="platina" type="number">${jogo.platina}</i></li>
+                <li><i class="bi bi-trophy-fill" id="ouro" type="number>${jogo.ouro}</i></li>
+                <li><i class="bi bi-trophy-fill" id="prata" type="number">${jogo.prata}</i></li>
+                <li><i class="bi bi-trophy-fill" id="bronze" type="number">${jogo.bronze}</i></li>
+            </ul>
+        </p>
         <span class="badge text-bg-warning">R$${jogo.valor},00</span>
         <p class="card-text">${jogo.estilo}</p>
     </div>
