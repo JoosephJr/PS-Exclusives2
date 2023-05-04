a73e377aef21ac5a1e8e2e10ba121c25480b28fd
JoosephJr <117952781+JoosephJr@users.noreply.github.com>
2023-05-03 21:02:12 -0300

    validation

--- js/crud.js
+++ js/crud.js
@@ -1,5 +1,21 @@
 document.querySelector("#salvar").addEventListener("click", cadastrar)
 
+let jogos = []
+
+
+
+window.addEventListener("load", () => {
+    jogos = JSON.parse(localStorage.getItem("jogos")) || []
+    if (jogos == null) jogos = []
+    atualizar()
+    
+} )
+
+function atualizar(){
+    jogos.forEach(jogo => 
+        document.querySelector("#jogos").innerHTML += criarCard(jogo))
+}
+
 function cadastrar() {
     const nome = document.querySelector("#nome").value
     const valor = document.querySelector("#valor").value
@@ -10,8 +26,9 @@ function cadastrar() {
     const ouro = document.querySelector("#tf-ouro").value
     const prata = document.querySelector("#tf-prata").value
     const bronze = document.querySelector("#tf-bronze").value
+    const modal = bootstrap.Modal.getInstance(document.querySelector("#exampleModal"))
 
-    const jogo = {
+    const jogo = { //JSON Java Script Object Notation
         nome,
         valor,
         descricao,
@@ -23,17 +40,36 @@ function cadastrar() {
         bronze
     }
 
-    document.querySelector("#jogos").innerHTML += criarCard(jogo)
+    if (!isValid(jogo.nome, document.querySelector("#nome"))) return
+    if (!isValid(jogo.valor, document.querySelector("#valor"))) return
+
+    jogos.push(jogo)
+    localStorage.setItem("jogos", JSON.stringify(jogos))
+
+    atualizar()
+    modal.hide()
+}
+
+function isValid(valor, campo){
+    if(valor.length == 0){
+        campo.classList.add("is-invalid")
+        campo.classList.remove("is-valid")
+        return false
+    }else{
+        campo.classList.add("is-valid")
+        campo.classList.remove("is-invalid")
+        return true
+    }
 }
 
 function apagar(botao) {
     console.log(botao)
-    botao.parentNode.parentNode.parentNode.remove()
+    botao.parentNode.parentNode.remove()
 }
 
 function criarCard(jogo) {
     const card = `
-    <div class="card col-lg-3 col-md-6 col-sm-12 text-bg-dark p-3" style="background-image: url(${jogo.imagemfundo});">
+    <div class="card col-lg-3 col-md-6 col-sm-12 text-bg-dark p-3" style="background-image: url(${jogo.imagemfundo}); background-size: cover;">
     <div class="card-header" id="titulo-jogo">${jogo.nome}</div>
     <div class="card-body">
         <p class="card-text">${jogo.descricao}</p>
